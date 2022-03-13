d749c398c67fa09d84c2f8a8210ac548b6e68ab9
Nithyadevi <nithi2218@gmail.com>
2022-03-13 23:26:31 +1100

    partially finished

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+const scuberNum = 42;
+const distanceFromHqInBlocks = (block) => {
+    if(block > scuberNum) {
+    return block-scuberNum;
+    }
+    else if (scuberNum > block) {
+        return scuberNum - block;
+    }
+}
+
+const distanceFromHqInFeet = (feet) => {
+    const distance = distanceFromHqInBlocks() * 264;
+    return distance;
+}
+
+const distanceTravelledInFeet = (scuberNum, destination ) => {
+
+    if(scuberNum > destination) {
+const differenceInFeet = (scuberNum - destination) * 264;
+return differenceInFeet;
+} else  if(scuberNum < destination) {
+    const differenceInFeet = (destination - scuberNum) * 264;
+    return differenceInFeet;
+    }
+
+}
+
+const calculatesFarePrice = (start, end) => {
+
+    const distance = (start - end) * 264;
+    
+    if (distance <= 400) {
+    return 0;
+    }
+    else if(distance > 400 && distance < 2000) {
+        return (distance - 400) * 0.02;
+    } 
+    else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } 
+    else { 
+        return 'cannot travel that far'; }
+}
+
